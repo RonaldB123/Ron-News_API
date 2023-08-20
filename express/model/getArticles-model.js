924c8942d96c6d1547d27f91949e78a9c04af020
RonaldB123 <ronaldbeck@hotmail.co.uk>
2023-08-20 13:56:03 +0100

    removed comment

--- express/model/getArticles-model.js
+++ express/model/getArticles-model.js
@@ -37,11 +37,3 @@ exports.articleData = (query) =>{
         return rows;
     })      
 }
-
-// `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id)::INT AS comment_count
-//     FROM articles
-//     LEFT JOIN comments ON comments.article_id = articles.article_id
-//     WHERE topic = 'mitch'
-//     GROUP BY articles.article_id
-//     ORDER BY created_at DESC
-//     `
